e42f236b95f86f140d71a7902a9491d1ab542c8c
Honest R. Chirozva <hchirozvah@outlook.com>
2024-02-13 16:14:33 +0200

    group changes to null

--- scripts/config_env.sh
+++ scripts/config_env.sh
@@ -89,7 +89,7 @@ function configure_microk8s {
     microk8s config >$k8s_user_home/.kube/config
 
     log INFO "Activating group changes"
-    su - $USER
+    su - $USER >>/dev/null 2>&1
 
     log OK "Microk8s configuration complete"
 }
@@ -109,8 +109,7 @@ function install_prerequisites {
             sudo groupadd docker
             sudo usermod -aG docker $k8s_user
             log INFO "Activating group changes"
-            # newgrp docker
-            su - $USER
+            newgrp docker >>/dev/null 2>&1
         fi
 
         # Check if nc (netcat) is installed
